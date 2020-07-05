b61129022244282b1549b1b36ee35cb6c10b532b
MistRain-1 <401941971@qq.com>
2020-07-05 16:50:45 +0800

    更新

---
+++ Tafang/Assets/Scripts/TestCharacter.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+class TestCharacter : MonoBehaviour
+{
+    //public float range;
+    public float timefireCD;
+    float fireCD = 0;
+    GameObject closestEnemy;
+    public int cost;
+    public bool High = false;
+    public int power;
+    public int blood;
+    public int MaxStopCount;
+    public int StopCount = 0;
+    List<GameObject> enemies = new List<GameObject>();
+    GameObject EndPoint;
+    Animator Attack;
+    AudioSource AttackAudio;
+    public GameObject ButtonChara;
+    void Awake()
+    {
+        StopCount = 0;
+        if (GameManage.instance.Allcost - cost <= 0 || GameManage.instance.charaRange <= 0)
+        {
+            Destroy(gameObject);
+        }
+        if (transform.tag == "LowSolider")
+        {
+            GameObject[] Highblocks = GameObject.FindGameObjectsWithTag("Highblock");
+            foreach (GameObject highblock in Highblocks)
+            {
+                if (transform.position == highblock.transform.position)
+                    Destroy(gameObject);
+            }
+        }
+        if (transform.tag == "HighSolider")
+        {
+            GameObject[] Lowblocks = GameObject.FindGameObjectsWithTag("Lowblock");
+            foreach (GameObject lowblock in Lowblocks)
+            {
+                if (transform.position == lowblock.transform.position)
+                    Destroy(gameObject);
+            }
+        }
+        EndPoint = GameObject.Find("EndPoint");
+
+    }
+    void Start()
+    {
+   
+        GameManage.instance.SwitchtoSpawn(null);
+        AttackAudio = GetComponent<AudioSource>();
+        Attack = GetComponent<Animator>();
+        gameObject.GetComponent<SpriteRenderer>().sortingOrder = -(int)transform.position.y;
+        GameManage.instance.Allcost -= cost;
+        GameManage.instance.charaRange -= 1;
+
+        InvokeRepeating("LocknShoot", 0f, 0.4f);
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Enemy")
+        {
+            enemies.Add(col.gameObject);
+        }
+    }
+    //    void OnTriggerStay2D(Collider2D col)
+    //{
+
+    //    if (col.tag=="Enemy"&&fireCD <= 0f)
+    //    {
+    //        col.GetComponent<testEnemy>().TakeDamage(power);
+    //        fireCD = timefireCD;
+    //    }
+    //    fireCD -= Time.deltaTime;
+
+    //}
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.tag == "Enemy")
+        {
+            enemies.Remove(col.gameObject);
+        }
+    }
+    void LocknShoot()
+    {
+
+        float closestDis = Mathf.Infinity;
+        if (enemies.ToArray().Length != 0)
+        {
+            foreach (GameObject enemy in enemies)
+            {
+                float distanceToEnemy = Vector3.Distance(EndPoint.transform.position, enemy.transform.position);
+                if (distanceToEnemy < closestDis)
+                {
+                    closestDis = distanceToEnemy;
+                    closestEnemy = enemy;
+                }
+            }
+        }
+    }
+    void Update()
+    {
+
+
+        GetComponentInChildren<Slider>().value = blood;
+
+        if (enemies.ToArray().Length != 0&&closestEnemy == null)
+        {
+            enemies.Remove(closestEnemy.gameObject);
+        }
+        if(closestEnemy!=null)
+        {
+            if (fireCD <= 0.25f)
+                Attack.SetBool("Attacking", true);
+            else
+                Attack.SetBool("Attacking", false);
+
+            if (fireCD <= 0f)
+            {
+                AttackAudio.Play();
+                closestEnemy.GetComponent<TestEnemy>().TakeDamage(power);
+                fireCD = timefireCD;
+            }
+            fireCD -= Time.deltaTime;
+        }
+        if (enemies.ToArray().Length == 0)
+        {
+            Attack.SetBool("Attacking", false);
+        }
+       
+        if (blood <= 0)
+        {
+            GameManage.instance.charaRange++;
+            Destroy(gameObject);
+        }
+
+    }
+    public void TakeDamage(int damage)
+    {
+        blood -= damage;
+    }
+
+}
--- Tafang/Assets/Scripts/test.cs
+++ Tafang/Assets/Scripts/test.cs
@@ -1,18 +1,143 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class test : MonoBehaviour
+using UnityEngine.UI;
+public class Test : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed;
+    Transform target;
+    int waves;
+    public int blood;
+    public int power;
+    public float timefireCD;
+    float fireCD = 0;
+    float Pspeed;
+
+    //GameObject closestSolider;
     void Start()
     {
-        
+        Pspeed = speed;
+        target = WayPoint.points[0];
+        fireCD = timefireCD;
+        //InvokeRepeating("LocknShoot", 0f, 0.4f);
     }
+    //void LocknShoot()
+    //{
+    //    GameObject[] LowSoliders = GameObject.FindGameObjectsWithTag("LowSolider");
+    //    foreach (GameObject Solider in LowSoliders)
+    //    {
+    //        float distanceToEnemy = Vector3.Distance(transform.position, Solider.transform.position);
+    //        if (distanceToEnemy < 0.3f && Solider.GetComponent<Character>().StopCount < Solider.GetComponent<Character>().MaxStopCount)
+    //        {
+    //            Solider.GetComponent<Character>().StopCount++;
+    //            speed = 0;
+    //            closestSolider = Solider;
 
-    // Update is called once per frame
+    //        }
+    //        else
+    //        {
+    //            speed = Pspeed;
+    //        }
+    //    }
+    //}
     void Update()
     {
+        fireCD -= Time.deltaTime;
+
+        gameObject.GetComponent<SpriteRenderer>().sortingOrder = -(int)transform.position.y;
+        GetComponentInChildren<Slider>().value = blood;
+        Vector3 direction = target.position - transform.position;
+        transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+        if (Vector3.Distance(transform.position, target.position) <= 0.3f)
+        {
+            if (waves >= WayPoint.points.Length - 1)
+            {
+                GameManage.instance.deathCount++;
+                GameManage.instance.GoHomeCount--;
+                DestroyImmediate(gameObject, true);
+
+            }
+            waves++;
+            target = WayPoint.points[waves];
+        }
+
+
+        //if (fireCD <= 0f)
+        //{
+        //    closestSolider.GetComponent<Character>().blood -= power;
+        //    fireCD = timefireCD;
+        //}
+        //fireCD -= Time.deltaTime;
+
+
+        else if (blood <= 0 && speed != 0)
+        {
+            GameManage.instance.deathCount++;
+            Destroy(gameObject);
+        }
+
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "LowSolider" && col.GetComponent<Character>().StopCount < col.GetComponent<Character>().MaxStopCount)
+        {
+            col.GetComponent<Character>().StopCount++;
+            speed = 0;
+           
+        }
+
+
+    }
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if (speed == 0)
+        {
+            if (fireCD <= 0)
+            {
+                col.GetComponent<Character>().TakeDamage(power);
+                fireCD = timefireCD;
+            }
+
+            if (blood <= 0)
+            {
+                col.GetComponent<Character>().StopCount--;
+                GameManage.instance.deathCount++;
+                Destroy(gameObject);
+            }
+          
+        }
+
+
+    }
+    //IEnumerator EnamyAttack(GameObject col)
+    //{
+    //    if (fireCD <= 0)
+    //    {
+    //        col.GetComponent<Character>().TakeDamage(power);
+    //        fireCD = timefireCD;
+    //    }
         
+    //    if (blood <= 0 && speed == 0)
+    //    {
+    //        col.GetComponent<Character>().StopCount--;
+    //        GameManage.instance.deathCount++;
+    //        Destroy(gameObject);
+    //    }
+
+    //    yield return new WaitForSeconds(0);
+    //}
+    void OnTriggerExit2D(Collider2D col)
+    {
+        //if (col.tag == "LowSolider")
+        //{
+        //    col.GetComponent<Character>().StopCount--;
+        //}
+        speed = Pspeed;
+
+    }
+    public void TakeDamage(int damage)
+    {
+
+        blood -= damage;
     }
 }
